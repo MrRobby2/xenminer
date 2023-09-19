fdad2fc646660c0ef57cc139e6993d8ff26a63ff
Robert <mrrobby2@gmail.com>
2023-09-19 22:50:32 +0200

    Update syncnode.sh

--- syncnode.sh
+++ syncnode.sh
@@ -1,6 +1,14 @@
+#!/bin/bash
+# Download fresh script 
 clear
-rm syncnode.py
 curl -o syncnode.py -LJO https://github.com/jacklevin74/xenminer/raw/main/syncnode.py
 echo
-echo "Start sync"
-while sleep 60; do python3 syncnode.py YOUR_ETH_ADRESS; done
+
+if [ -z "$1" ]; then
+  echo "Please provide an address as an argument."
+  exit 1
+fi
+
+account="$1"
+echo "Starting synchronization using account: $account"
+while sleep 60; do python3 syncnode.py "$account"; done
