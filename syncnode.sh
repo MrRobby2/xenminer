27166b4506e3934561ab9a12c18bd2c44e3ba5ad
Robert <mrrobby2@gmail.com>
2023-10-31 22:26:08 +0100

    Update syncnode.sh

--- syncnode.sh
+++ syncnode.sh
@@ -2,6 +2,7 @@
 # Download fresh script
 clear
 curl -o syncnode.py -LJO https://github.com/jacklevin74/xenminer/raw/main/syncnode.py
+curl -o listener.py -LJO https://github.com/jacklevin74/xenminer/raw/main/utils/listener.py
 echo
 
 if [ -z "$1" ]; then
@@ -15,3 +16,4 @@ session="session_node"
 
 echo "Starting synchronization using account: $account"
 screen -dmS "$session" bash -c "while sleep 60; do python3 syncnode.py \"$account\"; done"
+screen -S "$session" -X screen bash -c "python3 listener.py"
